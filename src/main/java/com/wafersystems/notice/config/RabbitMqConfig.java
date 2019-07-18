3d5bf336ae2d521d9876f3ace147cf6b0fdd19e6
tandongkui <tandongkui@wafersystems.com>
2019-07-18 18:16:59 +0800

    加入MQ，实现监听MQ消息进行发送邮件、短信功能；

---
+++ src/main/java/com/wafersystems/notice/config/RabbitMqConfig.java
@@ -0,0 +1,89 @@
+package com.wafersystems.notice.config;
+
+import com.wafersystems.virsical.common.core.constant.NoticeMqConstants;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * rabbitmq配置
+ *
+ * @author tandk
+ * @date 2019/6/11 11:09
+ */
+@Configuration
+public class RabbitMqConfig {
+
+  /**
+   * 配置消息转换器转Json
+   *
+   * @return 消息转换器
+   */
+  @Bean
+  public MessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+  /**
+   * 通知服务邮件队列
+   */
+  public static final String QUEUE_NOTICE_MAIL = "queue.notice.mail";
+
+  /**
+   * 通知服务短信队列
+   */
+  public static final String QUEUE_NOTICE_SMS = "queue.notice.sms";
+
+  /**
+   * DirectExchange
+   *
+   * @return DirectExchange
+   */
+  @Bean
+  public DirectExchange noticeDirectExchange() {
+    return new DirectExchange(NoticeMqConstants.EXCHANGE_DIRECT_NOTICE);
+  }
+
+  /**
+   * 邮件队列
+   *
+   * @return Queue
+   */
+  @Bean
+  public Queue queueNoticeMail() {
+    return new Queue(QUEUE_NOTICE_MAIL);
+  }
+
+  /**
+   * 短信队列
+   *
+   * @return Queue
+   */
+  @Bean
+  public Queue queueNoticeSms() {
+    return new Queue(QUEUE_NOTICE_SMS);
+  }
+
+  /**
+   * 绑定邮件队列到交换机
+   *
+   * @return Binding
+   */
+  @Bean
+  public Binding mailQueueBinding() {
+    return BindingBuilder.bind(queueNoticeMail()).to(noticeDirectExchange()).with(NoticeMqConstants.ROUTINT_KEY_MAIL);
+  }
+
+  /**
+   * 绑定短信队列到交换机
+   *
+   * @return Binding
+   */
+  @Bean
+  public Binding smsQueueBinding() {
+    return BindingBuilder.bind(queueNoticeSms()).to(noticeDirectExchange()).with(NoticeMqConstants.ROUTINT_KEY_SMS);
+  }
+
+}
