3112faede0873932d2932721b8a7407375eb722d
shennen <shennan@wafersystems.com>
2019-11-21 10:16:36 +0800

    SmsUtil去除魔法值，优化日志

---
+++ src/main/java/com/wafersystems/notice/constants/SmsConstants.java
@@ -0,0 +1,31 @@
+package com.wafersystems.notice.constants;
+
+/**
+ * 短信常量
+ *
+ * @author shennan
+ * @date 2019/11/21 9:07
+ */
+public class SmsConstants {
+
+  /**
+   * 中国电话区号
+   */
+  public static String CHINA_AREA_CODE = "+86";
+
+  /**
+   * 区号前缀
+   */
+  public static String AREA_CODE_PREFIX = "+";
+
+  /**
+   * https
+   */
+  public static String HTTPS = "https";
+
+  /**
+   * 成功码
+   */
+  public static Integer SUCCESS_CODE = 200;
+
+}
