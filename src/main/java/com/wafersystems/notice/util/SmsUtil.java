8feab7cefa8a88ba38c37e591de38e78d85dd8b9
shennen <shennan@wafersystems.com>
2019-11-21 10:16:36 +0800

    SmsUtil去除魔法值，优化日志

--- src/main/java/com/wafersystems/notice/util/SmsUtil.java
+++ src/main/java/com/wafersystems/notice/util/SmsUtil.java
@@ -1,5 +1,6 @@
 package com.wafersystems.notice.util;
 
+import com.wafersystems.notice.constants.SmsConstants;
 import com.wafersystems.security.SecurityUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
@@ -14,7 +15,6 @@ import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,54 +26,51 @@ import java.util.Map;
 public class SmsUtil {
 
   /**
+   * 发送短信
+   *
    * @param templetId 模板ID
-   * @param phoneNum 短信号码
-   * @param params 参数
+   * @param phoneNum  短信号码
+   * @param params    参数
    * @throws IOException IOException
    */
   public static String sendSms(String templetId, String phoneNum, List<String> params,
-      String domain, String smsSign) {
-    // String url = "https://work.virsical.cn/sms/ability";
-    // String clientId = "meeting";
-    // String secret = "23cbf2b615184418";
+                               String domain, String smsSign) {
+    log.debug("发短信信息.templetId={},phoneNum={},params={},domain={},smsSign={}",
+      templetId, phoneNum, params, domain, smsSign);
     String url = ParamConstant.getURL_SMS_SERVER();
     String clientId = ParamConstant.getURL_SMS_CLIENTID();
     String secret = ParamConstant.getURL_SMS_SECRET();
-    log.info("短信42:{}",url);
+    log.info("短信接口服务为:{}", url);
     url += '/' + clientId + '/' + secret;
     String prefix = "";
-    if (phoneNum.indexOf("+") != -1) {
-      if (!"+86".equals(phoneNum.substring(0, 3))) {
+    if (phoneNum.contains(SmsConstants.AREA_CODE_PREFIX)) {
+      if (!SmsConstants.CHINA_AREA_CODE.equals(phoneNum.substring(0, 3))) {
         prefix = phoneNum.substring(0, 3);
       }
       phoneNum = phoneNum.substring(3);
     }
     Map<String, String> hashMap = new HashMap<String, String>();
-    hashMap.put("calleeNbr", phoneNum);// 不支持带+86的
+    // 不支持带+86的
+    hashMap.put("calleeNbr", phoneNum);
     hashMap.put("templetId", templetId);
     hashMap.put("domain", StringUtils.isBlank(domain) ? ParamConstant.getDEFAULT_DOMAIN() : domain);
     hashMap.put("value1", params.get(0));
-    if (params.size() > 1 && StringUtils.isNotBlank(params.get(1))) {
-      hashMap.put("value2", params.get(1));
+    for (int i = 1; i < params.size(); i++) {
+      if (StringUtils.isNotBlank(params.get(i))) {
+        int index = i + 1;
+        hashMap.put("value" + index, params.get(i));
+      }
     }
-    if (params.size() > 2 && StringUtils.isNotBlank(params.get(2))) {
-      hashMap.put("value3", params.get(2));
-    }
-    if (params.size() > 3 && StringUtils.isNotBlank(params.get(3))) {
-      hashMap.put("value4", params.get(3));
-    }
-    if (params.size() > 4 && StringUtils.isNotBlank(params.get(4))) {
-      hashMap.put("value5", params.get(4));
-    }
-    if (params.size() > 5 && StringUtils.isNotBlank(params.get(5))) {
-      hashMap.put("value6", params.get(5));
-    }
-    hashMap.put("smsSign", smsSign);// 签名
-    hashMap.put("suffix", "false");// 签名是否前缀
-    hashMap.put("timestamp", System.currentTimeMillis() + "");// 必须和服务器时间差三分钟以内的时间戳才能通过验证
+    // 签名
+    hashMap.put("smsSign", smsSign);
+    // 签名是否前缀
+    hashMap.put("suffix", "false");
+    // 必须和服务器时间差三分钟以内的时间戳才能通过验证
+    hashMap.put("timestamp", System.currentTimeMillis() + "");
     String privateKey = "47a31cf3b5bea2e4e9a12659f4181283";
     String sign = "";
-    hashMap.put(privateKey, "");// 私钥
+    // 私钥
+    hashMap.put(privateKey, "");
     if (StringUtils.isNotBlank(prefix)) {
       // 国际短信使用
       hashMap.put("countorycode", prefix);
@@ -81,42 +78,47 @@ public class SmsUtil {
     } else {
       url += "/send?sign=";
     }
+    log.debug("短信参数为{}", hashMap);
     sign = SecurityUtils.calSignatureMap(hashMap);
     url = url + sign;
-    log.info("短信56:{}",url);
+    log.info("发送短信的服务接口为:{}", url);
     HttpResponse response = null;
-    if (url.startsWith("https")) {
+    if (url.startsWith(SmsConstants.HTTPS)) {
       try {
         HttpPost method = new HttpPost(url);
         method.addHeader("Content-type", "application/json; charset=utf-8");
         method.setHeader("Accept", "application/json");
-        hashMap.remove(privateKey);// 调用前删除私钥
+        // 调用前删除私钥
+        hashMap.remove(privateKey);
         method.setEntity(new StringEntity(new ObjectMapper().writeValueAsString(hashMap),
-            Charset.forName("UTF-8")));
+          Charset.forName("UTF-8")));
         HttpClient httpsClient = new SslClient();
         response = httpsClient.execute(method);
       } catch (Exception ex) {
-        ex.printStackTrace();// 异常信息
-        log.info("短信100:{}",ex.getMessage());
+        // 异常信息
+        ex.printStackTrace();
+        log.info("发送https短信异常:{}", ex.getMessage());
       }
     } else {
       try {
         HttpPost method = new HttpPost(url);
         method.addHeader("Content-type", "application/json; charset=utf-8");
         method.setHeader("Accept", "application/json");
-        hashMap.remove(privateKey);// 调用前删除私钥
+        //调用前删除私钥
+        hashMap.remove(privateKey);
         method.setEntity(new StringEntity(new ObjectMapper().writeValueAsString(hashMap),
-            Charset.forName("UTF-8")));
+          Charset.forName("UTF-8")));
         HttpClient httpClient = new DefaultHttpClient();
         response = httpClient.execute(method);
       } catch (Exception ex) {
-        ex.printStackTrace();// 异常信息
-        log.info("异常114:{}",ex.getMessage());
+        // 异常信息
+        ex.printStackTrace();
+        log.info("发送其它类型短信异常:{}", ex.getMessage());
       }
     }
 
     if (response != null) {
-      if (response.getStatusLine() != null && response.getStatusLine().getStatusCode() == 200) {
+      if (response.getStatusLine() != null && response.getStatusLine().getStatusCode() == SmsConstants.SUCCESS_CODE) {
         return "0";
       } else {
         // 失败
@@ -124,11 +126,13 @@ public class SmsUtil {
         try {
           log.error(EntityUtils.toString(response.getEntity()));
         } catch (ParseException pe) {
-          pe.printStackTrace();// 异常信息
-          log.info("短信128:{}",pe.getMessage());
+          // 异常信息
+          pe.printStackTrace();
+          log.info("发送短信异常:{}", pe.getMessage());
         } catch (IOException io) {
-          io.printStackTrace();// 异常信息
-          log.info("短信131:{}",io.getMessage());
+          // 异常信息
+          io.printStackTrace();
+          log.info("发送短信异常:{}", io.getMessage());
         }
       }
     }
