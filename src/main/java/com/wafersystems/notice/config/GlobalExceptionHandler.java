8b2c68c3bbf082c6e250de420776d704184b8931
tandongkui <tandongkui@wafersystems.com>
2020-03-04 18:46:28 +0800

    Security 统一处理接口参数无效异常，解决安全扫描问题：JSON 中反映的未清理用户输入

--- src/main/java/com/wafersystems/notice/config/GlobalExceptionHandler.java
+++ src/main/java/com/wafersystems/notice/config/GlobalExceptionHandler.java
@@ -5,9 +5,14 @@ import com.wafersystems.virsical.common.core.exception.BusinessException;
 import com.wafersystems.virsical.common.core.util.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 
 /**
@@ -46,6 +51,23 @@ public class GlobalExceptionHandler {
   public R handleBusinessException(BusinessException e) {
     return R.builder()
       .msg(e.getLocalizedMessage())
+      .code(CommonConstants.FAIL)
+      .build();
+  }
+
+  /**
+   * validation Exception
+   *
+   * @param exception exception
+   * @return R
+   */
+  @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public R handleBodyValidException(BindException exception) {
+    List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+    log.error("参数绑定异常,ex = {}", fieldErrors.get(0).getDefaultMessage());
+    return R.builder()
+      .msg("101001")
       .code(CommonConstants.FAIL)
       .build();
   }
