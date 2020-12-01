85979c119cc71ac333e5dcd2da72758c4e183608
shennen <shennan@wafersystems.com>
2020-12-01 15:06:57 +0800

    Changed 单元测试调整

--- src/test/java/com/wafersystems/notice/controller/SmsSendControllerTest.java
+++ src/test/java/com/wafersystems/notice/controller/SmsSendControllerTest.java
@@ -5,6 +5,8 @@ import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
 import com.wafersystems.notice.BaseTest;
 import com.wafersystems.notice.constants.ParamConstant;
+import com.wafersystems.notice.model.SmsTemplateDTO;
+import com.wafersystems.notice.model.TemplateStateUpdateDTO;
 import com.wafersystems.virsical.common.core.constant.CommonConstants;
 import com.wafersystems.virsical.common.core.dto.SmsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -67,6 +69,46 @@ public class SmsSendControllerTest extends BaseTest {
     params.add("phone", "15529360324");
     params.add("business", "123");
     JSONObject jsonObject = doGet(url, true, false, params);
+    Assert.assertEquals(jsonObject.get("code"), CommonConstants.SUCCESS);
+  }
+
+  @Test
+  public void testTemplatePage() throws Exception {
+    String url = "/sms/template/page";
+    JSONObject jsonObject = doGet(url, true, false);
+    Assert.assertEquals(jsonObject.get("code"), CommonConstants.SUCCESS);
+  }
+
+  @Test
+  public void testTemplateAdd() throws Exception {
+    String url = "/sms/template/add";
+    final SmsTemplateDTO template = new SmsTemplateDTO();
+    template.setId("55555");
+    template.setName("测试模板");
+    template.setContent("我是要给测试模板");
+    template.setDescription("描述");
+    template.setCategory("通用");
+    final String jsonString = JSONObject.toJSONString(template);
+    JSONObject jsonObject = doPost(url, jsonString, null, false, true);
+    Assert.assertEquals(jsonObject.get("code"), CommonConstants.SUCCESS);
+  }
+
+  @Test
+  public void testTemplateDel() throws Exception {
+    String url = "/sms/template/del/55555";
+    JSONObject jsonObject = doGet(url, false, true);
+    Assert.assertEquals(jsonObject.get("code"), CommonConstants.SUCCESS);
+  }
+
+  @Test
+  public void testUpdateState() throws Exception {
+    String url = "/sms/template/update/state";
+    final TemplateStateUpdateDTO dto = new TemplateStateUpdateDTO();
+    dto.setState(0);
+    dto.setUpdateAll(true);
+    dto.setId("");
+    final String jsonString = JSONObject.toJSONString(dto);
+    JSONObject jsonObject = doPost(url, jsonString, null, false, true);
     Assert.assertEquals(jsonObject.get("code"), CommonConstants.SUCCESS);
   }
 }
