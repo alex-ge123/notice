d5f66679acbd40ab760984e50325c067c177f0e8
tandongkui <tandongkui@wafersystems.com>
2021-01-12 14:40:24 +0800

    Changed 版本升级5.2.2，新增扩展参数多租户配置SQL脚本

---
+++ src/main/resources/db/migration/V5.2.2__virsical_notice.sql
@@ -0,0 +1,4 @@
+-- 修改ntc_parameter加入租户id支持多租户配置
+alter table `ntc_parameter` add column `tenant_id` int(11) not null default 0 commit '租户id';
+-- 将现有配置作为通用默认配置
+update `ntc_parameter` set tenant_id = 0 where tenant_id != 0;
