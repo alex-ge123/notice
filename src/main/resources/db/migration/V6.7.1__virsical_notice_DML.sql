b6a5bba8b69830e8bc4ea9be34a0f805e949f4b0
shennen <shennan@wafersystems.com>
2021-05-28 14:18:53 +0800

    Changed 升级版本、添加提醒数据库表结构

---
+++ src/main/resources/db/migration/V6.7.1__virsical_notice_DML.sql
@@ -0,0 +1,3 @@
+INSERT INTO `alert_conf`(`id`, `tenant_id`, `type`, `state`, `create_time`, `update_time`) VALUES (1, 0, 1, 1, '2021-05-28 14:13:58', NULL);
+INSERT INTO `alert_conf`(`id`, `tenant_id`, `type`, `state`, `create_time`, `update_time`) VALUES (2, 0, 2, 1, '2021-05-28 14:14:05', NULL);
+INSERT INTO `alert_conf`(`id`, `tenant_id`, `type`, `state`, `create_time`, `update_time`) VALUES (3, 0, 3, 1, '2021-05-28 14:14:11', NULL);
